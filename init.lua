1b5916cdaf659114c886f4095cb5027bcc4d1b9f
RedPenguin88 <ethanweihunt@gmail.com>
2022-06-20 18:07:18 -0700

    use lua rather than vimscript

---
+++ init.lua
@@ -0,0 +1,16 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.incsearch = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
