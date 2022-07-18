03af5b35b6e03dd918a7e64ab018bd991eeec042
RedPenguin88 <ethanweihunt@gmail.com>
2022-07-18 09:14:17 -0700

    set keybinding to clear highlights

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,3 +9,6 @@ keymap("n", "<C-h>", "<C-w>h", { silent = true })
 keymap("n", "<C-j>", "<C-w>j", { silent = true })
 keymap("n", "<C-k>", "<C-w>k", { silent = true })
 keymap("n", "<C-l>", "<C-w>l", { silent = true })
+
+-- Clear highlights when searching
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", { silent = true })
