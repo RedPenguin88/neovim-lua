6a0cd598314c7f1cb656d1594c65d164e78fbbcc
RedPenguin88 <ethanweihunt@gmail.com>
2022-07-20 09:40:37 -0700

    add markdown autocmd

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,8 +1,20 @@
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+-- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
+  pattern = '*',
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
-  pattern = '*',
+  group = highlight_group
+})
+
+local filetype_group = vim.api.nvim_create_augroup('FiletypeGroup', { clear = true})
+-- Set wrap and spell in markdown
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+  group = filetype_group
 })
