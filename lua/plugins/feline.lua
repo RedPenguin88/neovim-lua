a926058247367f3d52a349ef214b8d1b73141805
RedPenguin88 <ethanweihunt@gmail.com>
2022-08-02 08:50:02 -0700

    change colorscheme and custom components

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -1,7 +1,152 @@
-local ok, feline = pcall(require, "feline")
-if not ok then
+local bar_ok, feline = pcall(require, "feline")
+local color_ok, colors = pcall(require, "kanagawa.colors")
+if not (bar_ok and color_ok) then
   return
 end
 
-feline.setup()
-feline.winbar.setup()
+local default_colors = colors.setup()
+
+local vi_mode = require("feline.providers.vi_mode")
+
+local vi_mode_colors = {
+  NORMAL = default_colors.crystalBlue,
+  INSERT = default_colors.autumnGreen,
+  VISUAL = default_colors.oniViolet,
+  OP = default_colors.crystalBlue,
+  BLOCK = default_colors.oniViolet,
+  LINES = default_colors.oniViolet,
+  REPLACE = default_colors.autumnRed,
+  ['V-REPLACE'] = default_colors.autumnRed,
+  ENTER = default_colors.sakuraPink,
+  MORE = default_colors.sakuraPink,
+  SELECT = default_colors.surimiOrange,
+  COMMAND = default_colors.boatYellow2,
+  SHELL = default_colors.crystalBlue,
+  TERM = default_colors.crystalBlue,
+  NONE = default_colors.fujiGray
+}
+
+local comps = {
+  vi_mode = {
+    provider = function()
+      return "  " .. vi_mode.get_vim_mode()
+    end,
+    hl = function()
+      return {
+        name = vi_mode.get_mode_highlight_name(),
+        fg = vi_mode.get_mode_color(),
+      }
+    end,
+    right_sep = " "
+  },
+  file_info = {
+    provider = {
+      name = "file_info",
+      opts = {
+        file_readonly_icon = '  ',
+      }
+    },
+    hl = {
+      fg = default_colors.crystalBlue,
+      style = "bold"
+    }
+  },
+  git = {
+    branch = {
+      provider = "git_branch",
+    },
+    added = {
+      provider = "git_diff_added",
+      hl = {
+        fg = default_colors.autumnGreen
+      }
+    },
+    changed = {
+      provider = "git_diff_changed",
+      hl = {
+        fg = default_colors.autumnYellow
+      }
+    },
+    removed = {
+      provider = "git_diff_removed",
+      hl = {
+        fg = default_colors.autumnRed
+      }
+    }
+  },
+  lsp_name = {
+    provider = "lsp_client_names",
+    left_sep = " ",
+    right_sep = " ",
+    hl = {
+      fg = default_colors.boatYellow2
+    }
+  },
+  os = {
+    provider = function()
+      local icons = {
+        ["UNIX"] = " ",
+        ["MAC"] = " ",
+        ["DOS"] = " "
+      }
+      local os = vim.bo.fileformat:upper()
+      return icons[os] .. os
+    end,
+    right_sep = " ",
+    hl = {
+      fg = default_colors.sakuraPink,
+      style = "bold"
+    }
+  },
+  position = {
+    provider = "position",
+    right_sep = " ",
+    hl = {
+      fg = default_colors.oniViolet
+    }
+  },
+  line_percentage = {
+    provider = "line_percentage",
+    right_sep = " ",
+    hl = {
+      style = "bold"
+    }
+  },
+  scroll_bar = {
+    provider = "scroll_bar",
+    hl = {
+      fg = default_colors.crystalBlue,
+      style = "bold"
+    }
+  }
+}
+
+local components = {
+  active = {},
+  inactive = {}
+}
+
+table.insert(components.active, {})
+table.insert(components.active, {})
+table.insert(components.active, {})
+table.insert(components.inactive, {})
+table.insert(components.inactive, {})
+table.insert(components.inactive, {})
+
+table.insert(components.active[1], comps.vi_mode)
+table.insert(components.active[1], comps.file_info)
+table.insert(components.active[3], comps.git.branch)
+table.insert(components.active[3], comps.git.added)
+table.insert(components.active[3], comps.git.changed)
+table.insert(components.active[3], comps.git.removed)
+table.insert(components.active[3], comps.lsp_name)
+table.insert(components.active[3], comps.os)
+table.insert(components.active[3], comps.position)
+table.insert(components.active[3], comps.line_percentage)
+table.insert(components.active[3], comps.scroll_bar)
+
+feline.setup {
+  colors = { bg = colors.bg, fg = colors.fg },
+  vi_mode_colors = vi_mode_colors,
+  components = components
+}
