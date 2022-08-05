0edcb66dcb5b42982070be43819203412c27b17a
RedPenguin88 <ethanweihunt@gmail.com>
2022-08-05 09:34:51 -0700

    quit nvimtree upon exiting

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -42,3 +42,16 @@ vim.api.nvim_create_autocmd({"Filetype"}, {
   end,
   group = quit_group
 })
+
+local nvim_tree_group = vim.api.nvim_create_augroup("TreeGroup", { clear = true })
+-- Quit nvim-tree if it is the last buffer
+-- WARNING: If you quit without saving while NvimTree is open, neovim will crash!
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end,
+  group = nvim_tree_group
+})
