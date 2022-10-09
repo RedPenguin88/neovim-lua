ca4ebc81a54b5139edc308358aaa1290874ebf9d
Ethan Hunt <ethanweihunt@gmail.com>
2022-10-09 10:57:00 -0700

    disable dim_inactive

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,32 +5,33 @@ end
 
 catppuccin.setup {
   term_colors = true,
-  dim_inactive = { enabled = true },
+  dim_inactive = { enabled = false },
   integrations = {
     cmp = true,
     gitsigns = true,
     nvimtree = true,
     telescope = true,
     treesitter = true,
-		indent_blankline = {
-			enabled = true,
-			colored_indent_levels = false,
-		},
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = { "italic" },
-				hints = { "italic" },
-				warnings = { "italic" },
-				information = { "italic" },
-			},
-			underlines = {
-				errors = { "undercurl" },
-				hints = { "undercurl" },
-				warnings = { "undercurl" },
-				information = { "undercurl" },
-			},
-		},
+    markdown = true,
+    indent_blankline = {
+      enabled = true,
+      colored_indent_levels = false,
+    },
+    native_lsp = {
+      enabled = true,
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
+      },
+    },
   },
 }
 
