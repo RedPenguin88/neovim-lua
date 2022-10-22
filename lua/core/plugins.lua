727edb9fa081b90d92e69b73bbe969c6e03dbc45
Ethan Hunt <ethanweihunt@gmail.com>
2022-10-22 13:49:50 -0700

    remove cinnamon.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -157,11 +157,6 @@ return packer.startup({function(use)
     "goolord/alpha-nvim",
     config = function() require("plugins.alpha") end
   }
-  -- Smooth scrolling
-  use {
-    "declancm/cinnamon.nvim",
-    config = function() require("plugins.cinnamon") end
-  }
   -- LaTeX
   use {
     "lervag/vimtex",
--- lua/plugins/cinnamon.lua
+++
@@ -1,8 +0,0 @@
-local ok, cinnamon = pcall(require, "cinnamon")
-if not ok then
-  return
-end
-
-cinnamon.setup {
-  extra_keymaps = true
-}
